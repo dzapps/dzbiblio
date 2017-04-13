b680c936e57eeef419ba1823d6f05f0e3b4a5037
alidz1982 <alidz1982@users.noreply.github.com>
2017-04-13 20:54:48 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 config = {
     basepath: '/~alexrose/markdown/', // With first slash and trailing slash
-    server: 'http://step.polymtl.ca', // Without trailing slash
+    server: 'https://dzapps.github.io/dzbiblio/', // Without trailing slash
     contentPath: 'content/'           // With trailing slash
 };
 
